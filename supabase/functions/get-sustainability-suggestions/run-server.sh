24195a53c57d9bcc51b410602a6887af71c18933
Steven Jenkins <stvnjnkns101@gmail.com>
2025-05-12 21:24:15 +0000

    Fix test failures by adding import map to run-server.sh and run-tests.sh

--- supabase/functions/get-sustainability-suggestions/run-server.sh
+++ supabase/functions/get-sustainability-suggestions/run-server.sh
@@ -5,7 +5,7 @@ echo "Starting sustainability suggestions API server..."
 echo "Press Ctrl+C to stop the server"
 
 # Run the server using Deno
-deno run --allow-net --allow-env index.ts
+deno run --allow-net --allow-env --import-map=import_map.json index.ts
 
 # Note: If you need to specify additional permissions or import maps, add them here
 # Example: deno run --allow-net --allow-env --import-map=import_map.json index.ts
